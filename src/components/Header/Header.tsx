9f1616f750ed01492eb2b6dbc1de6c401600351a
sockki <poohct327@gmail.com>
2024-08-12 23:16:53 +0900

    Feat: Footer 컴포넌트 퍼블리싱

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,10 @@ import headerLogo from '../../../public/icons/headerLogo.svg';
 
 export default function Header() {
   return (
-    <div className="fixed top-0 w-full h-[13.7rem] flex justify-between items-center backdrop-blur-sm z-50">
+    <header
+      title="Header"
+      className="fixed top-0 w-full h-[13.7rem] flex justify-between items-center backdrop-blur-sm z-50"
+    >
       <div className="ml-[8rem] flex items-center">
         <Image src={headerLogo} alt="logo" />
         <span className="ml-[10rem] font-[500] text-[1.8rem] text-[#3f3f3f]">취약점 DB</span>
@@ -11,6 +14,6 @@ export default function Header() {
       <div className="">
         <span className="mr-[8rem] font-[500] text-[1.8rem] text-[#3f3f3f]">MY 저장소</span>
       </div>
-    </div>
+    </header>
   );
 }
