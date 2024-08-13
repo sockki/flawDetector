387d6d23a782ce4c55b9b947cdb3d57f50d24c53
sockki <poohct327@gmail.com>
2024-08-13 17:42:11 +0900

    Feat: Pagination 컴포넌트 퍼블리싱

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       title="Header"
       className="fixed top-0 w-full h-[13.7rem] flex justify-between items-center backdrop-blur-sm z-50"
     >
-      <div className="ml-[8rem] flex items-center">
+      <div className="ml-[8rem] flex items-center text-">
         <Image src={headerLogo} alt="logo" />
         <span className="ml-[10rem] font-[500] text-[1.8rem] text-[#3f3f3f]">취약점 DB</span>
       </div>
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
+import Image from 'next/image';
+import rightArrow from '../../../public/icons/rightArrow.svg';
+
+type PaginationProps = {
+  nowPage: number;
+  totalPage: number;
+  setNowPage: React.Dispatch<React.SetStateAction<number>>;
+  name: string;
+};
+
+export default function Pagination({ nowPage, totalPage, setNowPage, name }: PaginationProps) {
+  const [currentPage, setCurrentPage] = useState<number>(nowPage);
+  const router = useRouter();
+  const firstNum = currentPage - (((currentPage - 1) % 10) + 1) + 1;
+  return (
+    <div className="flex w-fit gap-[0.8rem]">
+      <button
+        type="button"
+        className="flex justify-center items-center w-[2rem] h-[2rem] bg-none text-[1.4rem] font-[400]"
+        onClick={() => {
+          setNowPage(nowPage - 1);
+          setCurrentPage(nowPage - 1);
+          router.push(`/${name}/${nowPage - 1}`);
+        }}
+        disabled={nowPage === 1}
+      >
+        <Image src={rightArrow} className="rotate-180" alt="leftArrow" />
+      </button>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i =>
+        firstNum + i <= totalPage ? (
+          <button
+            type="button"
+            key={i + 1}
+            className={twMerge(
+              'flex justify-center items-center w-[2rem] h-[2rem] bg-none text-[1.4rem] font-[400]',
+              firstNum + i === nowPage ? 'text-primary-300' : '',
+            )}
+            onClick={() => {
+              setNowPage(firstNum + i);
+              router.push(`/${name}/${firstNum + i}`);
+            }}
+          >
+            {firstNum + i}{' '}
+          </button>
+        ) : null,
+      )}
+      <button
+        className="flex justify-center items-center w-[2rem] h-[2rem] bg-none text-[1.4rem] font-[400]"
+        type="button"
+        onClick={() => {
+          setNowPage(nowPage + 1);
+          setCurrentPage(nowPage + 1);
+          router.push(`/${name}/${nowPage + 1}`);
+        }}
+        disabled={nowPage === totalPage}
+      >
+        <Image src={rightArrow} alt="rightArrow" />
+      </button>
+    </div>
+  );
+}
