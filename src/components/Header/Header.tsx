8ec9686c7bf1486bb55d52aea32ab330f0be5e12
sockki <poohct327@gmail.com>
2024-08-14 21:50:40 +0900

    Fix: Header 수정 사항 반영

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
       className="fixed top-0 w-full h-[13.7rem] flex justify-between items-center backdrop-blur-sm z-50"
     >
       <div className="ml-[8rem] flex items-center text-">
-        <Image src={headerLogo} alt="logo" />
+        <h1>
+          <Image src={headerLogo} alt="logo" />
+        </h1>
         <span className="ml-[10rem] font-[500] text-[1.8rem] text-[#3f3f3f]">취약점 DB</span>
       </div>
-      <div className="">
-        <span className="mr-[8rem] font-[500] text-[1.8rem] text-[#3f3f3f]">MY 저장소</span>
-      </div>
+      <div className="mr-[8rem] font-[500] text-[1.8rem] text-[#3f3f3f]">MY 저장소</div>
     </header>
   );
 }
