6fc2614f036373fc62ee54a153f8162a0b9d06df
sockki <poohct327@gmail.com>
2024-08-14 10:48:12 +0900

    Fix: tailwind.config.ts 수정

--- src/components/Pagination/TestPagination.tsx
+++ src/components/Pagination/TestPagination.tsx
@@ -7,3 +7,4 @@ export default function TestPagination({ params }: { params: { id: string } }) {
   const [nowPage, setNowPage] = useState<number>(Number(params.id));
   return <Pagination nowPage={nowPage} setNowPage={setNowPage} name="min" totalPage={20} />;
 }
+// pagination 사용 예시 파일
