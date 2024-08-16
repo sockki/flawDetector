890ab0c8538e266dc6fb7f9b49607c9b4e333201
Young2un <popduddms@gmail.com>
2024-08-17 02:31:14 +0900

    Feat: Suggest컴포넌트 구현

---
+++ src/components/Chips/SuggestionChip.tsx
@@ -0,0 +1,21 @@
+import { twMerge } from 'tailwind-merge';
+
+type SuggestionChipProps = {
+  variant: 'new' | 'hot' | 'warm' | 'notification' | 'report';
+  label: string;
+  isActive?: boolean;
+};
+export default function SuggestionChip({ variant, label, isActive = true }: SuggestionChipProps) {
+  const baseClasses =
+    'py-[0.8rem] px-[1.2rem] rounded-full bg-[#e8e8e8] text-gray-dark font-bold text-[1.6rem] inline-block';
+
+  const variantClasses = {
+    new: 'text-white bg-system-assist',
+    hot: 'text-white bg-system-warning',
+    warm: 'text-system-warning bg-red-light',
+    notification: 'text-primary-500 bg-primary-50',
+    report: 'text-gray-default bg-gray-light',
+  };
+
+  return <span className={twMerge(baseClasses, isActive && variantClasses[variant])}>{label}</span>;
+}
