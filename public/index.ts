a7cf983c2666cde461dfdc3a4ae0dee2e46d775b
Young2un <popduddms@gmail.com>
2024-08-17 04:41:53 +0900

    Fix: 빌드에러 잡아보겠슴다

--- public/index.ts
+++ public/index.ts
@@ -1,3 +1,4 @@
 export { default as CaretIcon } from './icons/caret.svg';
 export { default as DocumentIcon } from './icons/document.svg';
+export { default as LogoIcon } from './icons/headerLogo.svg';
 export { default as RemoveIcon } from './icons/remove.svg';
