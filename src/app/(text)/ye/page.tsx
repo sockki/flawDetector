5c0bb390be86bfb54f18cce35339362062223361
Young2un <popduddms@gmail.com>
2024-08-16 19:33:23 +0900

    Feat: assist chip 구현

--- src/app/(text)/ye/page.tsx
+++ src/app/(text)/ye/page.tsx
@@ -1,9 +1,16 @@
+import AssistChip from '@/components/Chips/AssistChip';
 import InputChip from '@/components/Chips/InputChip';
 
 export default function TestPage() {
   return (
     <>
       <div>TestPage</div>
+      <AssistChip variant="outline" label="label" />
+      <AssistChip variant="elevated" label="label" />
+      <AssistChip variant="outline-primary" label="label" />
+      <AssistChip variant="outline" label="label" disabled />
+      <AssistChip variant="elevated" label="label" disabled />
+      <AssistChip variant="outline-primary" label="label" disabled />
       <InputChip label=".eslintrc.json" />
       <InputChip label=".eslintrc.json" leftIcon percentage={20} isRemovable />
       <InputChip label=".eslintrc.json" leftIcon isRemovable />
