c997c56451326997ae706165c4164611f0585cd1
Young2un <popduddms@gmail.com>
2024-08-18 18:45:48 +0900

    Feat: 모달의 배경이 되는 Root 컴포넌트 구현

---
+++ src/components/Modals/ModalRoot.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+import Dimmed from '../common/Dimmed';
+
+type ModalRootProps = {
+  children: ReactNode;
+  gap: number;
+};
+
+export default function ModalRoot({ children, gap }: ModalRootProps) {
+  const gapSize = gap * 0.1;
+  return (
+    <>
+      <Dimmed />
+      <div
+        className={twMerge(
+          'absolute left-1/2 top-1/2 z-[100] flex -translate-x-1/2 -translate-y-1/2 transform flex-col bg-white p-[4.8rem]',
+        )}
+        style={{ gap: `${gapSize}rem` }}
+      >
+        {children}
+      </div>
+    </>
+  );
+}
