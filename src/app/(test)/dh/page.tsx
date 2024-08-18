fd18a654a0786467c323a56ecf296c4d5de45c63
chaduhwan <enghks2973@naver.com>
2024-08-18 19:50:08 +0900

    Feat: ProgressBar 컴포넌트 구현

---
+++ src/app/(test)/dh/page.tsx
@@ -0,0 +1,9 @@
+import { ProgressBar } from '@/components/ProgressBar/ProgressBar';
+
+export default function Dh() {
+  return (
+    <div>
+      <ProgressBar progress={70} />
+    </div>
+  );
+}
