8fafd06a22b2026121a1ecf0710242442a130d62
sockki <poohct327@gmail.com>
2024-08-19 13:54:10 +0900

    Fix: Pagination 컴포넌트 수정

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,54 +1,52 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
 
 type PaginationProps = {
   nowPage: number;
   totalPage: number;
-  name: string;
 };
 
-export default function Pagination({ nowPage, totalPage, name }: PaginationProps) {
-  const pageNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+export default function Pagination({ nowPage, totalPage }: PaginationProps) {
+  const pathname = usePathname();
   const router = useRouter();
-  const firstNum = nowPage - (((nowPage - 1) % 10) + 1) + 1;
+  const firstNumber = nowPage - (((nowPage - 1) % 10) + 1) + 1;
   return (
-    <div className="flex w-fit gap-[0.8rem]">
+    <div className="flex w-fit gap-[1.3rem]">
       <button
         type="button"
-        className="flex h-[2rem] w-[2rem] items-center justify-center bg-none text-[1.4rem] font-[400]"
+        className="flex h-[3.6rem] w-[3.6rem] items-center justify-center bg-none text-[1.6rem] font-regular text-gray-black hover:bg-purple-light"
         onClick={() => {
-          router.push(`/${name}/${nowPage - 1}`);
+          router.push(`${pathname}?page=${nowPage - 1}`);
         }}
         disabled={nowPage === 1}
       >
         <PaginationArrow className="rotate-180" />
       </button>
-      {pageNumber.map(number =>
-        firstNum + number <= totalPage ? (
-          <button
-            type="button"
-            key={number + 1}
-            className={twMerge(
-              'flex justify-center items-center w-[2rem] h-[2rem] bg-none text-[1.4rem] font-[400]',
-              firstNum + number === nowPage ? 'text-primary-300' : '',
-            )}
-            onClick={() => {
-              router.push(`/${name}/${firstNum + number}`);
-            }}
-          >
-            {firstNum + number}{' '}
-          </button>
-        ) : null,
+      {Array.from({ length: 10 }, (_, number) => number).map(
+        pageNumber =>
+          firstNumber + pageNumber <= totalPage && (
+            <button
+              type="button"
+              key={pageNumber + 1}
+              className={twMerge(
+                'flex h-[3.6rem] w-[3.6rem] items-center justify-center bg-none text-[1.6rem] font-regular text-gray-black hover:bg-purple-light',
+                firstNumber + pageNumber === nowPage ? 'text-primary-300' : '',
+              )}
+              onClick={() => {
+                router.push(`${pathname}?page=${firstNumber + pageNumber}`);
+              }}
+            >
+              {firstNumber + pageNumber}
+            </button>
+          ),
       )}
       <button
-        className="flex h-[2rem] w-[2rem] items-center justify-center bg-none text-[1.4rem] font-[400]"
+        className="flex h-[3.6rem] w-[3.6rem] items-center justify-center bg-none text-[1.6rem] font-regular text-gray-black hover:bg-purple-light"
         type="button"
         onClick={() => {
-          router.push(`/${name}/${nowPage + 1}`);
+          router.push(`${pathname}?page=${nowPage + 1}`);
         }}
         disabled={nowPage === totalPage}
       >
