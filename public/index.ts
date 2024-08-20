157dda119ab50d07fcbe861eaea5be87bdad6e51
Young2un <popduddms@gmail.com>
2024-08-20 23:52:23 +0900

    Feat: 아이콘 추가 index

--- public/index.ts
+++ public/index.ts
@@ -1,3 +1,4 @@
+export { default as BugIcon } from './icons/bug.svg';
 export { default as CaretIcon } from './icons/caret.svg';
 export { default as DocumentIcon } from './icons/document.svg';
 export { default as LogoIcon } from './icons/headerLogo.svg';
