067a24dfb5093758dafd104fc63b9172b28a21e0
Young2un <popduddms@gmail.com>
2024-08-21 03:11:46 +0900

    Style: 딤드 z인덱스 변경

--- src/components/common/Dimmed.tsx
+++ src/components/common/Dimmed.tsx
@@ -1,6 +1,6 @@
 function Dimmed() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60" />
+    <div className="fixed inset-0 z-[99] flex items-center justify-center bg-black bg-opacity-60" />
   );
 }
 
