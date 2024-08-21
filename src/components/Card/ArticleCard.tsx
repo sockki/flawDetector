4daeca0a90cf945c27c835286561b8cceb553d1a
sockki <poohct327@gmail.com>
2024-08-21 19:31:58 +0900

    Fix: Header,Footer,Card 수정

--- src/components/Card/ArticleCard.tsx
+++ src/components/Card/ArticleCard.tsx
@@ -23,11 +23,7 @@ export default function ArticleCard({
   content,
   date,
 }: ArticleCardProps) {
-  const nowDate = Date.now();
-  const timeDifference =
-    (nowDate - date.getTime()) / 1000 < 60
-      ? '방금 전'
-      : formatDistanceToNowStrict(date, { addSuffix: true, locale: ko });
+  const timeDifference = formatDistanceToNowStrict(date, { addSuffix: true, locale: ko });
   return (
     <article
       className={twMerge(
