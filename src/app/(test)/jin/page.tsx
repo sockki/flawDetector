ee5640b756ed8fa33fe48aa5cfb2f50f8157a3db
printjin <printjin@gmail.com>
2024-08-22 20:23:14 +0900

    Chore: Floating 컴포넌트 수정

--- src/app/(test)/jin/page.tsx
+++ src/app/(test)/jin/page.tsx
@@ -2,10 +2,18 @@
 
 import { FloatingButton } from '../../../components/Floating/FloatingButton';
 
+const generateContent = (count: number) => {
+  const content = [];
+  for (let i = 1; i <= count; i += 1) {
+    content.push(<p key={i}>{i}</p>);
+  }
+  return content;
+};
+
 export default function HomeApp() {
   return (
     <div>
-      <div className="p-4">a</div>
+      <div className="p-4">{generateContent(100)}</div>
 
       <FloatingButton type="top" />
       {/* <FloatingButton type="ask" /> */}
