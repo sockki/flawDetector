90beecd1793cb6eaa0a3730aca1605ed5ba21a57
printjin <printjin@gmail.com>
2024-08-22 21:38:36 +0900

    Fix: min/test에서  name 속성 제거하여 타입 오류 수정

--- src/app/(test)/min/[id]/page.tsx
+++ src/app/(test)/min/[id]/page.tsx
@@ -3,7 +3,7 @@ import Pagination from '@/components/Pagination/Pagination';
 export default function Page({ params }: { params: { id: string } }) {
   return (
     <div>
-      <Pagination nowPage={Number(params.id)} name="min" totalPage={20} />
+      <Pagination nowPage={Number(params.id)} totalPage={20} />
     </div>
   );
 }
