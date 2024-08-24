f167db45e60fcd1db6021f07c9d0b9c4d2bcb737
Young2un <popduddms@gmail.com>
2024-08-25 02:59:08 +0900

    Feat: dropdown

--- src/app/(test)/ye/page.tsx
+++ src/app/(test)/ye/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import Button from '@/components/Button/Button';
+import FilterChip from '@/components/Chips/FilterChip';
 import { auth } from '@/firebase/firebaseClient';
 import { signInWithGithub } from '@/utils/signInWithGitHub';
 import { useEffect, useState } from 'react';
 
-export default function GithubButton() {
-  const [isLogin, setIsLogin] = useState(false);
+const options = ['option1', 'optixdasdasokd;laon2'];
 
+export default function TestPage() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [option, setOption] = useState('');
+
+  const handleSelect = (v: string) => {
+    setOption(() => v);
+  };
+  console.log(option);
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setIsLogin(!!user);
@@ -20,6 +28,7 @@ export default function GithubButton() {
       <Button onClick={signInWithGithub}>로그인 버튼</Button>
       <Button onClick={() => auth.signOut()}>로그아웃 버튼</Button>
       <div>{isLogin ? 'Logged in' : 'Logged out'}</div>
+      <FilterChip label="type" options={options} onSelect={handleSelect} hasIcon />
     </>
   );
 }
