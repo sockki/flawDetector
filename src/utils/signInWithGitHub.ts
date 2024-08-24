af925cb56b3a5cc9d6c14cb7052dd2d25fb6bb14
Young2un <popduddms@gmail.com>
2024-08-24 23:32:07 +0900

    Feat: 로그인 로직 통합

--- src/utils/signInWithGitHub.ts
+++ src/utils/signInWithGitHub.ts
@@ -3,12 +3,16 @@ import { GithubAuthProvider, signInWithPopup } from 'firebase/auth';
 import { addDoc, collection } from 'firebase/firestore';
 
 export const signInWithGithub = async () => {
-  const provider = new GithubAuthProvider();
-  const { user } = await signInWithPopup(auth, provider);
-  await addDoc(collection(db, 'users'), {
-    uid: user.uid,
-    email: user.email,
-    name: user.displayName,
-    photo: user.photoURL,
-  });
+  try {
+    const provider = new GithubAuthProvider();
+    const { user } = await signInWithPopup(auth, provider);
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      email: user.email,
+      name: user.displayName,
+      photo: user.photoURL,
+    });
+  } catch (error) {
+    throw error;
+  }
 };
