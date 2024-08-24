44128b53ab18350275ea5edc3a8ce6b2804d93c7
Young2un <popduddms@gmail.com>
2024-08-25 00:07:40 +0900

    Test: 로그인 사용예시 및 상태 확인 방법

--- src/app/(test)/ye/page.tsx
+++ src/app/(test)/ye/page.tsx
@@ -1,5 +1,25 @@
-import Login from './_components/Login';
+'use client';
+
+import Button from '@/components/Button/Button';
+import { auth } from '@/firebase/firebaseClient';
+import { signInWithGithub } from '@/utils/signInWithGitHub';
+import { useEffect, useState } from 'react';
 
 export default function GithubButton() {
-  return <Login />;
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setIsLogin(!!user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+  return (
+    <>
+      <Button onClick={signInWithGithub}>로그인 버튼</Button>
+      <Button onClick={() => auth.signOut()}>로그아웃 버튼</Button>
+      <div>{isLogin ? 'Logged in' : 'Logged out'}</div>
+    </>
+  );
 }
