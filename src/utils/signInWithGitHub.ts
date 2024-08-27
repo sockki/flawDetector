0b5d7250100c86fb254f68ecad9958297a853ecb
Young2un <popduddms@gmail.com>
2024-08-28 03:28:55 +0900

    Fix: 데이터베이스에 중복저장 막기

--- src/utils/signInWithGitHub.ts
+++ src/utils/signInWithGitHub.ts
@@ -1,17 +1,23 @@
 import { auth, db } from '@/firebase/firebaseClient';
 import { GithubAuthProvider, signInWithPopup } from 'firebase/auth';
-import { addDoc, collection } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 export const signInWithGithub = async () => {
   try {
     const provider = new GithubAuthProvider();
     const { user } = await signInWithPopup(auth, provider);
-    await addDoc(collection(db, 'users'), {
-      uid: user.uid,
-      email: user.email,
-      name: user.displayName,
-      photo: user.photoURL,
-    });
+
+    const userRef = doc(db, 'users', user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (!userSnap.exists()) {
+      await setDoc(userRef, {
+        uid: user.uid,
+        email: user.email,
+        name: user.displayName,
+        photo: user.photoURL,
+      });
+    }
   } catch (error) {
     throw error;
   }
