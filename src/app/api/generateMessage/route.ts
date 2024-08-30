e69cf6670a2b53f623a2b3d97baa795a0f2c293a
Young2un <popduddms@gmail.com>
2024-08-30 09:43:57 +0900

    Feat: generateMessage 라우트핸들러 구현

--- src/app/api/generateMessage/route.ts
+++ src/app/api/generateMessage/route.ts
@@ -1,49 +1,13 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-// app/api/generateMessage/route.ts
+import { postGenerateMessage } from '@/apis/llama';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  const { user_message, temperature, top_p } = await request.json();
+  try {
+    const { prompt } = await request.json();
+    const generatedMessage = await postGenerateMessage(prompt);
 
-  // 토큰 발급
-  const authResponse = await fetch('http://43.203.238.76:8000/auth/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      username: 'sfacspace_3',
-      password: '3836734a5a59fb0c',
-    }),
-  });
-
-  if (!authResponse.ok) {
-    console.log(authResponse);
-    return NextResponse.json({ error: 'Authentication failed' }, { status: 401 });
+    return NextResponse.json({ message: generatedMessage });
+  } catch (error) {
+    return NextResponse.json({ error: '메세지 생성 실패' }, { status: 500 });
   }
-
-  const { access_token } = await authResponse.json();
-
-  // LLaMA3 API 호출
-  const generateResponse = await fetch('http://43.203.238.76:8000/generate', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      user_message,
-      temperature,
-      top_p,
-    }),
-  });
-
-  if (!generateResponse.ok) {
-    return NextResponse.json({ error: 'Failed to generate response' }, { status: 500 });
-  }
-
-  // const test = await generateResponse.json();
-  const data = await generateResponse.text();
-  console.log(generateResponse);
-  return NextResponse.json(data);
 }
