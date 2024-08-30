d48febf702509b6b2d33e020d15857412616b576
chaduhwan <enghks2973@naver.com>
2024-08-30 10:47:08 +0900

    Feat: 다이나믹 라우트로 변경

---
+++ src/app/repos/[repo_id]/_components/ScanEntireFolder.tsx
@@ -0,0 +1,9 @@
+import Button from '@/components/Button/Button';
+
+export function ScanEntireFolder() {
+  return (
+    <Button shape="rectangle" size="large" className="h-[10.7rem] w-[24.7rem]">
+      폴더 전체 검사
+    </Button>
+  );
+}
