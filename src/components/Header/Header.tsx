df1239ba4f5eda48fdb22b7243e0e4b4223f6dc3
sockki <poohct327@gmail.com>
2024-08-30 14:23:31 +0900

    Fix: Header 태그 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,29 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 flex h-[13.7rem] w-full items-center justify-between backdrop-blur-sm">
-      <div className="text- ml-[8rem] flex items-center">
-        <h1>
-          <Link href="/">
-            <LogoIcon />
-          </Link>
-        </h1>
-        <Link href="/vulnerability-db?page=1">
-          <span className="ml-[10rem] text-[1.8rem] font-[500] text-gray-black">취약점 DB</span>
+    <header className="sticky top-0 z-50 flex h-[13.7rem] w-full items-center backdrop-blur-sm">
+      <h1 className="text- ml-[8rem] flex items-center">
+        <Link href="/">
+          <LogoIcon />
         </Link>
-      </div>
-      <div className="mr-[8rem] text-[1.8rem] font-[500] text-gray-black">MY 저장소</div>
+      </h1>
+      <nav className="w-full">
+        <ul className="flex items-center justify-between">
+          <li className="ml-[10rem]">
+            <Link
+              href="/vulnerability-db?page=1"
+              className="text-[1.8rem] font-[500] text-gray-black"
+            >
+              취약점 DB
+            </Link>
+          </li>
+          <li className="mr-[8rem]">
+            <Link href="/repos" className="text-[1.8rem] font-[500] text-gray-black">
+              MY 저장소
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
