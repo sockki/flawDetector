d574d061782b639b6a5ef9656a2bf0e87d174739
bang-wol <milk011922@gmail.com>
2024-08-30 21:40:14 +0900

    Feat: 설정페이지 퍼블리싱

---
+++ src/app/me/setting/page.tsx
@@ -0,0 +1,18 @@
+import UserCard from '@/app/repos/_components/UserCard';
+import MyPageTitle from '../_components/MyPageTitle';
+import UserSetting from '../_components/UserSetting';
+
+export default function Page() {
+  const avatarUrl = '/images/AvatarDummyImg.png';
+  const email = 'marry@gmail.com';
+
+  return (
+    <div className="mx-auto flex min-h-screen w-[131.4rem] flex-col items-center gap-[12.4rem]">
+      <MyPageTitle title="Setting" />
+      <div className="flex w-full flex-col gap-[7.1rem]">
+        <UserCard avatar={avatarUrl} email={email} hasLogoutButton />
+        <UserSetting />
+      </div>
+    </div>
+  );
+}
