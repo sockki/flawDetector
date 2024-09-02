591b35101147b86766637b600028fb4965ef799c
bang-wol <milk011922@gmail.com>
2024-09-03 01:45:14 +0900

    Fix: 충돌 해결

--- src/app/(test)/min/page.tsx
+++ src/app/(test)/min/page.tsx
@@ -1,44 +1,71 @@
 'use client';
 
-import ArticleCard from '@/components/Card/ArticleCard';
-import ArticleSmallCard from '@/components/Card/ArticleSmallCard';
-import DBCard from '@/components/Card/DBCard';
-import DetectFileCard from '@/components/Card/DetectFileCard';
-import ScrapCard from '@/components/Card/ScrapCard';
+import ArticleCard from '@/components/VulDbCard/ArticleCard';
+import ArticleSmallCard from '@/components/VulDbCard/ArticleSmallCard';
+import DetectFileCard from '@/components/LibraryCard/DetectFileCard';
+import ScrapCard from '@/components/LibraryCard/ScrapCard';
 import Pagination from '@/components/Pagination/Pagination';
-import dummyImg from '@/public/images/DBCardDummyImg1.png';
+import dummyImg from '@/public/images/BannerCardDummyImg1.png';
+import BannerCard from '@/components/VulDbCard/BannerCard';
+import { useState } from 'react';
 
-export default function page({ searchParams }: { searchParams: { page: string } }) {
-  const dummyDate = new Date(2024, 7, 21, 12, 43, 0);
+export default function MinTextPage({ searchParams }: { searchParams: { page: string } }) {
+  const dummyDate = new Date(2024, 7, 21, 17, 16, 0);
+  const [isHoverFirstCard, setIsHoverFirstCard] = useState<boolean>(true);
   return (
     <div className="flex flex-col gap-[2rem] p-[1rem]">
       <div className="flex gap-[2.8rem]">
-        <DBCard
+        <BannerCard
+          id={1}
           date={dummyDate}
           title="2023년 12월 CNNVD 호환 서비스 신제품 발표"
           backgroundId={1}
+          isHoverFirstCard={isHoverFirstCard}
+          setIsHoverFirstCard={setIsHoverFirstCard}
         />
-        <DBCard
+        <BannerCard
+          id={2}
           date={dummyDate}
           title="2023년 12월 CNNVD 호환 서비스 신제품 발표"
           backgroundId={2}
+          isHoverFirstCard={isHoverFirstCard}
+          setIsHoverFirstCard={setIsHoverFirstCard}
         />
-        <DBCard
+        <BannerCard
+          id={3}
           date={dummyDate}
           title="2023년 12월 CNNVD 호환 서비스 신제품 발표"
           backgroundId={3}
+          isHoverFirstCard={isHoverFirstCard}
+          setIsHoverFirstCard={setIsHoverFirstCard}
         />
       </div>
       <div className="flex gap-[2.8rem]">
-        <DetectFileCard title="title" caption="caption" isDetected={false} />
-        <DetectFileCard title="title" caption="caption" isDetected />
+        <DetectFileCard title="Project-1" date={dummyDate} label="before" isBookmarked={false} />
+        <DetectFileCard title="Project-1" date={dummyDate} label="under" isBookmarked={false} />
+        <DetectFileCard title="Project-1" date={dummyDate} label="done" isBookmarked />
+      </div>
+      <div className="flex gap-[2.8rem]">
         <ScrapCard
           title="Microsoft의 여러 보안 취약점에 대한 CNNVD의 보고서 및 기타 취약점에 대한 보고서"
           date={dummyDate}
+          label="warning"
+        />
+        <ScrapCard
+          title="Microsoft의 여러 보안 취약점에 대한 CNNVD의 보고서 및 기타 취약점에 대한 보고서"
+          date={dummyDate}
+          label="notification"
+        />
+        <ScrapCard
+          title="Microsoft의 여러 보안 취약점에 대한 CNNVD의 보고서 및 기타 취약점에 대한 보고서"
+          date={dummyDate}
+          label="report"
         />
       </div>
+
       <div className="flex flex-col gap-[2.8rem]">
         <ArticleCard
+          id={1}
           label="hot"
           title="2023년 12월 CNNVD 호환 서비스 신제품 발표"
           company="Microsoft"
@@ -46,6 +73,7 @@ export default function page({ searchParams }: { searchParams: { page: string } 
           date={dummyDate}
         />
         <ArticleCard
+          id={2}
           label="new"
           title="2023년 12월 CNNVD 호환 서비스 신제품 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표"
           company="Microsoft"
@@ -56,12 +84,16 @@ export default function page({ searchParams }: { searchParams: { page: string } 
       </div>
       <div className="flex gap-[2.8rem]">
         <ArticleSmallCard
+          id={1}
+          company="Microsoft"
           label="hot"
           title="2023년 12월 CNNVD 호환 서비스 신제품 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표"
           content="그런 일은 그런 일은 그런 일은 그런 일은 그런 일은"
           date={dummyDate}
         />
         <ArticleSmallCard
+          id={2}
+          company="Microsoft"
           label="new"
           title="2023년 12월 CNNVD 호환 서비스 신제품 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표 발표"
           content="그런 일은 그런 일은 그런 일은 그런 일은 그런 일은"
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,21 +1,30 @@
 import * as ProgressPrimitive from '@radix-ui/react-progress';
 import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type ProgressBarProps = ComponentProps<typeof ProgressPrimitive.Root> & {
+  type: 'enabled' | 'analye' | 'waiting' | 'success' | 'error';
   progress?: 0 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100;
+  isError?: boolean;
 };
 
-export function ProgressBar({ progress = 0, ...rest }: ProgressBarProps) {
-  const rootStyles = 'relative h-[1.2rem] w-[139.2rem] overflow-hidden rounded-full bg-neutral-10';
+export function ProgressBar({ progress = 0, type, ...rest }: ProgressBarProps) {
+  const rootStyles = twMerge(
+    'relative overflow-hidden rounded-full bg-neutral-10 h-[0.4rem] w-[22.6rem]',
+  );
 
-  const indicatorStyles =
-    'h-full w-full rounded-full bg-system-success transition-transform duration-300';
+  const indicatorStyles = twMerge(
+    'h-full w-full rounded-full transition-transform duration-300',
+    type === 'error' ? 'bg-[#FFCECE]' : 'bg-[#17E0D4]',
+  );
 
   return (
     <ProgressPrimitive.Root value={progress} max={100} className={rootStyles} {...rest}>
       <ProgressPrimitive.Indicator
         className={indicatorStyles}
-        style={{ transform: `translateX(${progress - 100}%)` }}
+        style={{
+          transform: `translateX(${type === 'error' || type === 'success' ? 0 : progress - 100}%)`,
+        }}
       />
     </ProgressPrimitive.Root>
   );
