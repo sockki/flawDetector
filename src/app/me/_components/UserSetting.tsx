10042f1a8d2e217285f68a7182ea0ee7cfd10e8e
bang-wol <milk011922@gmail.com>
2024-09-02 23:41:17 +0900

    Refactor: 기본값인 flex-row 삭제

--- src/app/me/_components/UserSetting.tsx
+++ src/app/me/_components/UserSetting.tsx
@@ -8,13 +8,13 @@ export default function UserSetting() {
 
   return (
     <div className="flex min-h-screen w-full flex-col items-start gap-[7.1rem]">
-      <div className="flex w-full flex-row items-center gap-[4.8rem]">
+      <div className="flex w-full items-center gap-[4.8rem]">
         <p className="w-[9.4rem] text-[2.4rem] font-bold">계정 유형</p>
         <p className="text-[2.4rem] font-regular">깃허브 연동</p>
       </div>
-      <div className="flex w-full flex-row items-end gap-[4.8rem]">
+      <div className="flex w-full items-end gap-[4.8rem]">
         <p className="w-[9.4rem] text-[2.4rem] font-bold">알림</p>
-        <div className="flex flex-grow flex-row items-center justify-between">
+        <div className="flex flex-grow items-center justify-between">
           <p className="text-[2.4rem] font-regular">
             이메일로 알림 받기
             <span className="text-[2.4rem] font-regular text-gray-default">
--- src/app/repos/_components/RepositoryActions.tsx
+++ src/app/repos/_components/RepositoryActions.tsx
@@ -5,14 +5,14 @@ export default function RepositoryActions() {
     <section className="flex w-full gap-[2rem]">
       <button
         type="button"
-        className="flex w-full flex-row items-center justify-center gap-[1rem] rounded-[1.2rem] border border-neutral-10 bg-white p-[1.6rem] text-[2rem] font-medium text-gray-black"
+        className="flex w-full items-center justify-center gap-[1rem] rounded-[1.2rem] border border-neutral-10 bg-white p-[1.6rem] text-[2rem] font-medium text-gray-black"
       >
         <ClockCounterIcon />
         Recents File
       </button>
       <button
         type="button"
-        className="flex w-full flex-row items-center justify-center gap-[1rem] rounded-[1.2rem] border border-neutral-10 bg-white p-[1.6rem] text-[2rem] font-medium text-gray-black"
+        className="flex w-full items-center justify-center gap-[1rem] rounded-[1.2rem] border border-neutral-10 bg-white p-[1.6rem] text-[2rem] font-medium text-gray-black"
       >
         <FolderSimpleStarIcon />
         Bookmarks
