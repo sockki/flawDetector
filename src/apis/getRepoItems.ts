a9d04473c1b495a4c6b2a51aa3aba7f48fb632e1
chaduhwan <enghks2973@naver.com>
2024-09-02 10:50:01 +0900

    Feat: url 분기 삭제

--- src/apis/getRepoItems.ts
+++ src/apis/getRepoItems.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server';
+
 type GetRepoItemsProps = {
   userName: string;
   repoName: string;
@@ -6,11 +8,9 @@ type GetRepoItemsProps = {
 
 export async function getRepoItems({ userName, repoName, path = '' }: GetRepoItemsProps) {
   try {
-    const apiUrl = path
-      ? `https://api.github.com/repos/${userName}/${repoName}/contents/${path}`
-      : `https://api.github.com/repos/${userName}/${repoName}/contents`;
-
-    const res = await fetch(apiUrl);
+    const res = await fetch(
+      `https://api.github.com/repos/${userName}/${repoName}/contents/${path}`,
+    );
 
     if (!res.ok) {
       throw new Error(`레포지토리를 가져오는데 실패하였습니다: ${res.status} ${res.statusText}`);
@@ -18,7 +18,9 @@ export async function getRepoItems({ userName, repoName, path = '' }: GetRepoIte
 
     const repoItems = await res.json();
 
-    return repoItems;
+    const plainItems = new NextResponse(repoItems);
+
+    return plainItems;
   } catch (error) {
     console.error('레포지토리를 가져오는 중 에러가 발생했습니다.', error);
     return null;
