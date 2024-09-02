1a0c65a15e7f77201d98adbdf3ad9e8f31145efe
sockki <poohct327@gmail.com>
2024-09-02 11:59:01 +0900

    Fix: conflict 해결

--- public/index.ts
+++ public/index.ts
@@ -20,10 +20,13 @@ export { default as CrossIcon } from './icons/switchCross.svg';
 export { default as ChatIcon } from './icons/floatChat.svg';
 export { default as TopIcon } from './icons/floatVector.svg';
 export { default as PaginationArrowIcon } from "./icons/paginationArrow.svg"
-export { default as DBCardArrowIcon } from "./icons/dbCardArrow.svg"
-export { default as DetectFileCardButtonIcon } from "./icons/detectFileCardButton.svg"
 export { default as ExternalLinkIcon } from "./icons/externalLinkIcon.svg"
 export { default as PinIcon } from "./icons/pinIcon.svg"
 export { default as BigPinIcon } from "./icons/bigPinIcon.svg"
 export { default as ShareIcon } from "./icons/sharingIcon.svg"
 export { default as SpacSpaceLogoIcon } from "./icons/spacSpaceLogo.svg"
+export {default as BannerCardArrowIcon} from "./icons/bannerCardArrow.svg"
+export {default as DetectFileCardArrowIcon} from "./icons/detectFileCardArrow.svg"
+export {default as DetectFileCardBugIcon} from "./icons/detectFileCardBug.svg"
+export {default as DetectFileCardStar} from "./icons/detectFileCardStar.svg"
+export {default as SearchIcon} from "./icons/searchIcon.svg"
