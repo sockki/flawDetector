fc15132bc883a680c81d64e608f78ea119e33a64
sockki <poohct327@gmail.com>
2024-09-02 11:00:12 +0900

    Fix: ScrapCard if문 객체로 수정

--- src/components/LibraryCard/ScrapCard.tsx
+++ src/components/LibraryCard/ScrapCard.tsx
@@ -3,6 +3,13 @@ import { twMerge } from 'tailwind-merge';
 
 type ScrapLabelType = 'warning' | 'notification' | 'report';
 
+type ElementByLabel = {
+  [key in ScrapLabelType]: {
+    labelStyle: string;
+    labelText: string;
+  };
+};
+
 type ScrapCardProps = {
   title: string;
   date: Date;
@@ -10,26 +17,23 @@ type ScrapCardProps = {
 };
 
 export default function ScrapCard({ title, date, label }: ScrapCardProps) {
-  const labelText = (labelData: ScrapLabelType) => {
-    if (labelData === 'warning') return '취약성 경고';
-    else if (labelData === 'notification') return '취약성 알림';
-    else return '취약성 보고서';
+  const elementByLabel: ElementByLabel = {
+    warning: { labelStyle: 'bg-red-light text-system-warning', labelText: '취약성 경고' },
+    notification: { labelStyle: 'bg-primary-50 text-primary-500', labelText: '취약성 알림' },
+    report: { labelStyle: 'bg-gray-light text-gray-default', labelText: '취약성 보고서' },
   };
 
-  const labelStyle = (labelData: ScrapLabelType) => {
-    if (labelData === 'warning') return 'bg-red-light text-system-warning';
-    else if (labelData === 'notification') return 'bg-primary-50 text-primary-500';
-    else return 'bg-gray-light text-gray-default';
-  };
+  const { labelStyle, labelText } = elementByLabel[label];
+
   return (
     <div className="relative flex h-[21.7rem] w-[42.2rem] flex-col rounded-[0.8rem] border-[0.1rem] border-[#c3c3c3] bg-white p-[2.8rem]">
       <label
         className={twMerge(
           'flex h-[3.5rem] w-fit items-center justify-center rounded-full px-[1.2rem] py-[0.8rem] text-[1.6rem] font-regular leading-[1.936rem]',
-          labelStyle(label),
+          labelStyle,
         )}
       >
-        {labelText(label)}
+        {labelText}
       </label>
       <div className="mt-[0.8rem] flex h-[7.2rem] w-[35.566rem] items-center">
         <span className="line-clamp-2 text-wrap text-[2.4rem] font-medium leading-[3.6rem]">
