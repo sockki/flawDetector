9d057d25bd37827d395ec3a3c3eeacaf1803a7be
bang-wol <milk011922@gmail.com>
2024-09-02 22:45:36 +0900

    Refactor: div -> section 태그 수정

--- src/app/me/_components/ContactSection.tsx
+++ src/app/me/_components/ContactSection.tsx
@@ -3,9 +3,9 @@ import ContactForm from './ContactForm';
 
 export default function ContactSection() {
   return (
-    <div className="flex gap-[12.4rem]">
+    <section className="flex gap-[12.4rem]">
       <ContactInfo />
       <ContactForm />
-    </div>
+    </section>
   );
 }
