95d38d0c3715450358c9c5190fdb6af68e1301a8
printjin <printjin@gmail.com>
2024-09-05 01:01:50 +0900

    Fix: 타원 개수, 속도 수정 및 코드 리팩토링

--- src/components/Ellipse.tsx
+++ src/components/Ellipse.tsx
@@ -1,26 +1,18 @@
-export function Ellipse(): React.ReactElement {
-  const ellipses = [];
-  const totalEllipses = 16;
-
-  for (let i = 0; i < totalEllipses; i += 1) {
-    ellipses.push(
-      <div
-        key={i}
-        className="animate-spread"
-        style={{
-          animationDelay: `${i * 0.5}s`,
-          width: '16.6rem',
-          height: '5rem',
-          top: '48.788rem',
-          left: '87.741rem',
-          border: '0.1rem solid var(--Primary-purple-50, #F2EBFF)',
-          backgroundColor: 'transparent',
-          borderRadius: '50% / 50%',
-          position: 'absolute',
-        }}
-      />,
-    );
-  }
-
-  return <>{ellipses}</>;
+export function Ellipse() {
+  return (
+    <div className="absolute h-full w-screen overflow-hidden">
+      {Array.from({ length: 50 }).map((_, i) => {
+        const key = `line-${i}`;
+        return (
+          <div
+            key={key}
+            className="top absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 transform animate-spread rounded-l-[50%] rounded-r-[50%] border border-primary-50"
+            style={{
+              animationDelay: `${i * -0.5}s`,
+            }}
+          />
+        );
+      })}
+    </div>
+  );
 }
