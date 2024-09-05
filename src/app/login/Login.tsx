f23a8f09d669ea00bc0c5e98588f929c796bc28a
printjin <printjin@gmail.com>
2024-09-05 11:32:26 +0900

    Fix: ellipse 위치 수정 및 버튼 컴포넌트 변경

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -1,37 +1,36 @@
 'use client';
 
 import { Ellipse } from '@/components/Ellipse';
+import Button from '@/components/Button/Button';
 
 export default function UiLogin() {
-  return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center bg-white">
-      <Ellipse />
+  const handleButtonClick = () => {
+    console.log('Button clicked');
+  };
 
-      <div className="relative z-10 flex h-[20.3rem] w-[140.6rem] items-center justify-between">
+  return (
+    <div className="relative flex min-h-screen flex-col items-center justify-center">
+      <Ellipse className="left-1/2 top-0 -translate-x-1/2 transform" />
+
+      <div className="flex h-[20.3rem] w-[140.6rem] items-center justify-between">
         <div className="flex h-[20.3rem] w-[43.8rem] flex-col items-center gap-[2rem]">
           <h1 className="text-[6rem] text-primary-500">Find your Flaw,</h1>
-
-          <button
-            type="button"
-            className="flex h-[11rem] w-[24rem] items-center justify-center rounded-full border-2 border-primary-500 text-[6rem] text-primary-500"
-          >
-            Login
-          </button>
+          <Button onClick={handleButtonClick}>Login</Button>
         </div>
 
-        <button
-          type="button"
-          className="h-[5.6rem] w-[35.4rem] rounded-full bg-primary-500 text-[2.8rem] text-white"
+        <Button
+          onClick={handleButtonClick}
+          className="rounded-full bg-primary-500 text-[2.8rem] text-white"
         >
           Github로 연동 로그인하기
-        </button>
+        </Button>
 
-        <button
-          type="button"
-          className="h-[5.6rem] w-[12.2rem] rounded-full bg-primary-500 text-[2.4rem] text-white"
+        <Button
+          onClick={handleButtonClick}
+          className="rounded-full bg-primary-500 text-[2.4rem] text-white"
         >
           Github
-        </button>
+        </Button>
       </div>
     </div>
   );
