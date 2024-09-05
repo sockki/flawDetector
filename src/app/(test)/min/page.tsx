85c012df6f7ea79ed61b115a99f30859e9991dce
sockki <poohct327@gmail.com>
2024-09-05 09:42:27 +0900

    Feat: 크롤링 데이터 가져오기

--- src/app/(test)/min/page.tsx
+++ src/app/(test)/min/page.tsx
@@ -1,12 +1,14 @@
 export default async function page() {
   const html = await fetch('http://localhost:3000/api/vulDb', {
-    cache: 'no-cache',
+    cache: 'no-store',
   });
   const dataset = await html.json();
   console.log(dataset);
-  return (
-    <div>
-      <ul />
-    </div>
-  );
+  return {
+    /* <div className="flex flex-col">
+      <span>{dataset[0].title}</span>
+      <span>{dataset[0].subTitle}</span>
+      <ul>{dataset[0].content.map(c => typeof c === "string" ?  <li>{c}</li> : <ul className="flex flex-col gap-10">{c.table?.map((t,index) => <li>{JSON.stringify(t)}</li>)}</ul>)}</ul>
+    </div> */
+  };
 }
--- src/app/api/vulDb/route.ts
+++ src/app/api/vulDb/route.ts
@@ -1,42 +1,99 @@
-import { NextResponse } from 'next/server';
+/* import { NextResponse } from 'next/server';
 import os from 'os';
 import * as cheerio from 'cheerio';
 import puppeteer from 'puppeteer-core';
 import getExecutablePath from '@/libs/getExcutablePath';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '@/firebase/firebaseConfig';
+
+export type CrawlingData = {
+  title: string;
+  subTitle: string;
+  content: Content[];
+};
+
+type Content = string | Table;
+
+type Table = {
+  table?: { [key: string]: string }[];
+};
+
+function parseDetailContent($: cheerio.CheerioAPI): Content[] {
+  const content: Content[] = [];
+  $('.detail-content')
+    .children()
+    .each((_, element) => {
+      if ($(element).is('p')) {
+        const textContent = $(element).text().trim();
+        content.push(textContent);
+      } else if ($(element).is('table')) {
+        const rows = $(element).find('tr');
+
+        const tableData: { [key: string]: string }[] = [];
+
+        rows.each((__, row) => {
+          const rowData: { [key: string]: string } = {};
+          $(row)
+            .find('td, th')
+            .each((index, cell) => {
+              const cellContent = $(cell).text().trim();
+              rowData[`Column${index + 1}`] = cellContent;
+            });
+          tableData.push(rowData);
+        });
+        content.push({ table: tableData });
+      }
+    });
+
+  return content;
+}
 
 export async function GET() {
   let browser;
   try {
     browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
       executablePath: getExecutablePath(os.platform()),
     });
-    const page = await browser.newPage();
-    await page.goto('https://www.cnnvd.org.cn/home/warn');
-    await page.click('div.el-input');
-    await page.click('div.el-select-dropdown li:nth-child(4) span');
-    const html = await page.content();
-    const $ = cheerio.load(html);
-    const titles = $('.content-title')
-      .map((_, element) => {
-        return $(element).text();
-      })
-      .get();
 
-    const dates = $('div.content-detail span')
-      .map((_, element) => {
-        return $(element).text();
-      })
-      .get();
-    const data = [];
-    for (let i = 0; i < titles.length; i += 1) {
-      const item = {
-        id: i,
-        title: titles[i],
-        date: dates[i],
-      };
-      data.push(item);
+    const page = await browser.newPage();
+    await page.goto('https://www.cnnvd.org.cn/home/warn', {
+      waitUntil: 'networkidle2',
+    });
+
+    const data: CrawlingData[] = [];
+
+    for (let i = 0; i < 1; i += 1) {
+      for (let j = 0; j < 2; j += 1) {
+        await new Promise<void>(resolve => setTimeout(resolve, 2000));
+        await page.waitForSelector(
+          `#loudong > form > div.content > div > div.el-col.el-col-16 > div:nth-child(${j + 1})`,
+        );
+        await page.click(
+          `#loudong > form > div.content > div > div.el-col.el-col-16 > div:nth-child(${j + 1})`,
+        );
+        await new Promise<void>(resolve => setTimeout(resolve, 2000));
+        const html = await page.content();
+        const $ = cheerio.load(html);
+
+        const title = $('.detail-title').text();
+        const subTitle = $('div.detail-subtitle > span').text();
+        const content = parseDetailContent($);
+
+        await addDoc(collection(db,"vulDb"), {
+          title, subTitle, content
+        })
+        await page.waitForSelector('.el-page-header__left');
+        await page.click(`.el-page-header__left`);
+      }
+
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      await page.waitForSelector(`button.btn-next`);
+      await page.click(`button.btn-next`);
+ 
     }
-    return NextResponse.json(data);
+ 
+    return NextResponse.json({message: "Done", status: true});
   } catch (error) {
     if (error instanceof Error) {
       return Response.json({ message: error.message, status: false });
@@ -47,3 +104,4 @@ export async function GET() {
     }
   }
 }
+ */
