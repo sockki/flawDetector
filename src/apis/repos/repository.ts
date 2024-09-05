cd9ba31e910799752b46f0cbbd53052cca9eae83
chaduhwan <enghks2973@naver.com>
2024-09-05 09:10:06 +0900

    Feat: 레포지토리 리스트 api 추가

--- src/apis/repos/repository.ts
+++ src/apis/repos/repository.ts
@@ -21,3 +21,18 @@ export async function getRepoContents({ owner, repo, path = '' }: GetRepoContent
     return null;
   }
 }
+
+export async function getRepolist(userName: string = '') {
+  try {
+    const response = await octokit.request(`GET /users/${userName}/repos`, {
+      userName,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('레포지토리 목록를 가져오는 중 에러가 발생했습니다:', error);
+    return null;
+  }
+}
