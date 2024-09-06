536dbfb68d98219abdcee3ebaef632c88da90474
bang-wol <milk011922@gmail.com>
2024-09-06 20:15:26 +0900

    Feat: 북마크 버튼 상태 업데이트 추가

--- src/components/LibraryCard/DetectFileCard.tsx
+++ src/components/LibraryCard/DetectFileCard.tsx
@@ -1,14 +1,41 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { twMerge } from 'tailwind-merge';
 import { DetectFileCardArrowIcon, DetectFileCardBugIcon, DetectFileCardStar } from '@/public/index';
 import type { DetectFileCardProps, ElementByLabel } from '@/types/detectedFileCard';
-import { format } from 'date-fns';
-import { useState } from 'react';
-import { twMerge } from 'tailwind-merge';
 
-export default function DetectFileCard({ title, label, date, isBookmarked }: DetectFileCardProps) {
+async function updateBookmarkStatus(userId: string, repoId: string, isBookmarked: boolean) {
+  const response = await fetch('/api/repositories', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      userId,
+      repoId,
+      isBookmarked,
+    }),
+  });
+  if (!response.ok) {
+    throw new Error('북마크 상태 업데이트 실패');
+  }
+}
+
+export default function DetectFileCard({
+  userId,
+  userName,
+  repoId,
+  title,
+  label,
+  date,
+  isBookmarked,
+  setRepositories,
+}: DetectFileCardProps) {
   const [isBookmark, setIsBookmark] = useState<boolean>(isBookmarked);
-
+  const router = useRouter();
   const elementByLabel: ElementByLabel = {
     before: {
       labelStyle: '',
@@ -32,21 +59,33 @@ export default function DetectFileCard({ title, label, date, isBookmarked }: Det
 
   const { labelStyle, labelText, buttonStyle, buttonText } = elementByLabel[label];
 
-  const onClickBookmark = () => {
-    setIsBookmark(prev => !prev);
+  const onClickBookmark = async () => {
+    try {
+      setIsBookmark(prev => !prev);
+
+      await updateBookmarkStatus(userId, repoId, !isBookmark);
+
+      const res = await fetch(`/api/repositories?userId=${userId}`);
+      const updatedData = await res.json();
+      setRepositories(updatedData.repositories);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onClickCheckStatus = () => {
+    router.push(`/repos/${userName}/${repoId}`);
   };
 
   return (
-    <div className="group relative flex h-[22.5rem] w-[31rem] cursor-pointer flex-col justify-between rounded-[1.2rem] border-[0.1rem] border-primary-100 p-[2rem]">
-      <div
-        onClick={onClickBookmark}
-        className="absolute right-[2rem] flex h-[4.8rem] w-[4.8rem] items-center justify-center rounded-[1.2rem] group-hover:border-[0.2rem] group-hover:border-primary-200"
-      >
+    <div className="group relative flex h-[22.5rem] w-[31rem] cursor-pointer flex-col justify-between rounded-[1.2rem] border-[0.1rem] border-primary-100 bg-white p-[2rem]">
+      <div className="absolute right-[2rem] flex h-[4.8rem] w-[4.8rem] items-center justify-center rounded-[1.2rem] group-hover:border-[0.2rem] group-hover:border-primary-200">
         <DetectFileCardStar
           className={twMerge(
             'ml-[0.15rem] mt-[0.15rem] h-[2.8rem] w-[2.8rem] group-hover:stroke-primary-200 group-hover:stroke-1',
             isBookmark ? 'fill-primary-200 stroke-primary-200 stroke-1' : '',
           )}
+          onClick={onClickBookmark}
         />
       </div>
       <div className="flex flex-col gap-[0.4rem]">
@@ -71,6 +110,7 @@ export default function DetectFileCard({ title, label, date, isBookmarked }: Det
             'flex items-center gap-[0.7rem] rounded-[1.4rem] p-[1rem]',
             buttonStyle,
           )}
+          onClick={onClickCheckStatus}
         >
           <DetectFileCardBugIcon />
           <span className="text-[2rem] font-regular leading-[2.8rem] text-white">{buttonText}</span>
