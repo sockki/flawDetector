b4dbbf03137e3d9d5d85edfe8932378afaf73bd1
chaduhwan <enghks2973@naver.com>
2024-09-07 22:17:43 +0900

    Feat: 라우트 변경

--- src/app/repos/[repo_id]/_components/ScanButton.tsx
+++
@@ -1,9 +0,0 @@
-import Button from '@/components/Button/Button';
-
-export function ScanButton() {
-  return (
-    <Button shape="rectangle" size="large" className="w-[24.7rem]">
-      검사하기
-    </Button>
-  );
-}
