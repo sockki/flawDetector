e636527eeac5fbb78dc53fce6dc619d637c7aa02
Young2un <popduddms@gmail.com>
2024-09-10 00:08:11 +0900

    Refactor: hero컴포넌트 리펙토링

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -27,32 +27,26 @@ export default function Hero() {
   };
 
   return (
-    <div className="relative h-[102.4rem] w-full overflow-hidden">
+    <div className="relative w-full overflow-hidden">
       <Ellipse />
 
-      <section id="hero" className="min-h-full content-center">
+      <section id="hero" className="min-h-screen content-center">
         <div className="flex flex-col items-center gap-[6.5rem] text-primary-500">
           <div className="flex flex-col items-center justify-center gap-[4rem]">
-            <div className="flex flex-col items-center justify-center gap-[2rem]">
-              <h1 className="text-center text-[6rem] font-normal leading-[7.261rem] tracking-[0.015em]">
-                Find your Flaw,
-              </h1>
-              <div className="flex h-[11rem] w-[47rem] items-center justify-center rounded-full border-[0.4rem] border-primary-500 px-[4rem]">
-                <span className="text-center text-[6rem] font-normal leading-[7.261rem] tracking-[0.015em]">
-                  FlawDetector
-                </span>
+            <div className="flex flex-col items-center justify-center gap-[2rem] text-[6rem] leading-[7.261rem] tracking-[0.015em]">
+              <h2>Find your Flaw,</h2>
+              <div className="content-center rounded-full border-[0.4rem] border-primary-500 px-[4rem] py-[1.85rem]">
+                <span>FlawDetector</span>
               </div>
             </div>
-            <p className="text-center text-[2rem] font-normal leading-[2.42rem] tracking-[-0.01em]">
+            <p className="text-[2rem]">
               인공지능의 뛰어난 분석 능력을 활용하여 코드의 보안 취약점을 신속하게 해결하세요.
             </p>
           </div>
           <Button onClick={handleButtonClick}>
             {isAuthenticated ? '파일 분석하러 가기' : 'Login'}
           </Button>
-          <div className="flex h-[5.6rem] w-[5.6rem] items-center justify-center">
-            <LandingDownIcon />
-          </div>
+          <LandingDownIcon />
         </div>
       </section>
     </div>
