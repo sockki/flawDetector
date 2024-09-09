8619ad760da4b9f6c3f75fcdb5bf987aa2510017
Young2un <popduddms@gmail.com>
2024-09-10 02:36:27 +0900

    Refactor: 필터칩, 토픽 수정

--- src/components/Chips/FilterChip.tsx
+++ src/components/Chips/FilterChip.tsx
@@ -34,24 +34,25 @@ export default function FilterChip({ label, options, hasIcon, onSelect }: Filter
             className={`ml-[0.4rem] transition-transform ${isOpen ? 'rotate-180 transform' : ''}`}
           />
         )}
+
+        {isOpen && (
+          <ul className="absolute left-1/2 top-full z-50 mt-2 -translate-x-1/2 transform overflow-hidden rounded-[0.8rem] border border-[#c3c3c3] bg-white shadow-drop">
+            {options.map(option => (
+              <li
+                key={option}
+                className={twMerge(
+                  'flex cursor-pointer justify-center gap-[0.8rem] overflow-hidden text-nowrap px-[0.95rem] py-[1rem] text-gray-dark hover:bg-purple-light',
+                  selectedOption === option && 'bg-purple-50 text-gray-black',
+                )}
+                onClick={() => handleOptionClick(option)}
+              >
+                {selectedOption === option && <SelectedIcon />}
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-      {isOpen && (
-        <ul className="left-0 mt-2 overflow-hidden rounded-[0.8rem] border border-[#c3c3c3] bg-white shadow-drop">
-          {options.map(option => (
-            <li
-              key={option}
-              className={twMerge(
-                'flex cursor-pointer gap-[0.8rem] overflow-hidden text-nowrap px-[0.95rem] py-[1rem] text-gray-dark hover:bg-purple-light',
-                selectedOption === option && 'bg-purple-50 text-gray-black',
-              )}
-              onClick={() => handleOptionClick(option)}
-            >
-              {selectedOption === option && <SelectedIcon />}
-              {option}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 }
