0a47012855d6b5cd66a046ae524437142f577557
chaduhwan <enghks2973@naver.com>
2024-09-10 21:23:43 +0900

    Feat: Reposide 를 layout에 추가하고 page.tsx에서 제거

--- src/app/repos/[userName]/[repo_id]/page.tsx
+++ src/app/repos/[userName]/[repo_id]/page.tsx
@@ -1,15 +1,5 @@
-import { RepoSide } from './_components/RepoSide';
 import { ScanFormat } from './_components/ScanFormat';
 
-export default function RepositoryContents({
-  params,
-}: {
-  params: { userName: string; repo_id: string };
-}) {
-  return (
-    <div className="flex gap-[2.8rem]">
-      <RepoSide params={params} />
-      <ScanFormat />;
-    </div>
-  );
+export default function RepositoryContents() {
+  return <ScanFormat />;
 }
--- src/app/repos/[userName]/[repo_id]/repo_inspection/page.tsx
+++ src/app/repos/[userName]/[repo_id]/repo_inspection/page.tsx
@@ -1,15 +1,5 @@
-import { RepoSide } from '../_components/RepoSide';
 import { ResultFormat } from './_components/resultFormat';
 
-export default function RepoInspection({
-  params,
-}: {
-  params: { userName: string; repo_id: string };
-}) {
-  return (
-    <div className="flex gap-[2.8rem]">
-      <RepoSide params={params} />
-      <ResultFormat />
-    </div>
-  );
+export default function RepoInspection() {
+  return <ResultFormat />;
 }
