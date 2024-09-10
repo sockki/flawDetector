73969a879e7ded4d713b86b875e0a13082fd415f
chaduhwan <enghks2973@naver.com>
2024-09-10 21:23:03 +0900

    Feat: layout 동적 라우트 params 추가

--- src/app/repos/[userName]/[repo_id]/layout.tsx
+++ src/app/repos/[userName]/[repo_id]/layout.tsx
@@ -1,10 +1,20 @@
+import { ReactNode } from 'react';
 import { Title } from './_components/Title';
+import { RepoSide } from './_components/RepoSide';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface LayoutProps {
+  children: ReactNode;
+  params: { userName: string; repo_id: string };
+}
+
+export default function Layout({ children, params }: LayoutProps) {
   return (
     <div className="relative left-[8rem] flex h-[147.1rem] w-[176.05rem] flex-col gap-[2.0rem]">
       <Title />
-      {children}
+      <div className="flex gap-[2.8rem]">
+        <RepoSide params={params} />
+        {children}
+      </div>
     </div>
   );
 }
