8d4b2468b923bdf707d63e1f4006ce3e613f0183
sockki <poohct327@gmail.com>
2024-09-10 10:05:16 +0900

    Fix: build 에러 해결

--- src/app/(test)/jin/page.tsx
+++ src/app/(test)/jin/page.tsx
@@ -6,7 +6,6 @@ export default function HomeApp() {
   return (
     <div>
       <div className="h-[1000rem]">아무글자</div>
-      <FloatingButton type="ask" onClick={() => alert('Ask Button Clicked!')} />
       <FloatingButton type="top" />
     </div>
   );
