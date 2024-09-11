2bf96ce2b4d973bd0b2d583d004d26bbe6f0b894
bang-wol <milk011922@gmail.com>
2024-09-11 09:59:29 +0900

    Feat: 로그인 안 했을 경우 my저장소 페이지 추가

--- src/app/repos/page.tsx
+++ src/app/repos/page.tsx
@@ -1,13 +1,22 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import ReposPageHeader from '@/app/repos/_components/ReposPageHeader';
 import UserCard from '@/app/repos/_components/UserCard';
 import RepositoryList from '@/app/repos/_components/RepositoryList';
 import RepositoryActions from '@/app/repos/_components/RepositoryActions';
+import NonLoginReposPage from '@/app/repos/_components/NonLoginReposPage';
 
-export default function ReposPage({
+export default async function ReposPage({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return <NonLoginReposPage />;
+  }
+
   return (
     <div className="mx-auto bg-opacity-70 bg-[url('/images/headerBackground.svg')] bg-contain bg-top bg-no-repeat">
       <div className="mx-auto flex w-[131.4rem] min-w-[131.4rem] flex-col items-center gap-[12.4rem]">
