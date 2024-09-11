8545620ce191d9eae1e358fdcf9e862da258cad8
bang-wol <milk011922@gmail.com>
2024-09-11 09:58:48 +0900

    Refactor: 로그인 페이지 배경 추가 및 필요없는 코드 삭제

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,39 +1,38 @@
 'use client';
 
-import Button from '@/components/Button/Button';
-import { signIn, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import Button from '@/components/Button/Button';
+import { Ellipse } from '@/components/Ellipse';
 
 export default function LoginPage() {
-  const router = useRouter();
   const { status } = useSession();
-
-  useEffect(() => {
-    if (status === 'authenticated') {
-      router.push('/repos');
-    }
-  }, [status, router]);
 
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
 
+  const handleGithubLoginButton = () => {
+    signIn('github');
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center gap-[24.6rem] bg-[]">
-      <div className="flex flex-col items-center gap-[2rem]">
-        <h1 className="text-[6rem] font-regular text-primary-500">Find your Flaw,</h1>
-        <h2 className="w-fit rounded-full border-[0.4rem] border-primary-500 bg-white px-[4rem] text-[6rem] font-regular text-primary-500">
-          Login
-        </h2>
+    <div className="relative flex min-h-screen flex-col items-center justify-center">
+      <Ellipse />
+      <div className="flex min-h-screen items-center justify-center gap-[24.6rem]">
+        <div className="flex flex-col items-center gap-[2rem]">
+          <h1 className="text-[6rem] font-regular text-primary-500">Find your Flaw,</h1>
+          <h2 className="w-fit rounded-full border-[0.4rem] border-primary-500 bg-white px-[4rem] text-[6rem] font-regular text-primary-500">
+            Login
+          </h2>
+        </div>
+        <Button type="button" onClick={handleGithubLoginButton}>
+          GitHub로 연동 로그인하기
+        </Button>
+        <Link href="https://github.com/">
+          <Button>Github</Button>
+        </Link>
       </div>
-      <Button type="button" onClick={() => signIn('github')}>
-        GitHub로 연동 로그인하기
-      </Button>
-      <Link href="https://github.com/">
-        <Button>Github</Button>
-      </Link>
     </div>
   );
 }
