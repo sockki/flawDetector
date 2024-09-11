1f644c0bf0b1c1d20038bc30e47f887abc639d86
bang-wol <milk011922@gmail.com>
2024-09-11 14:31:58 +0900

    Refactor: 서버 컴포넌트로 데이터 가져오기

--- src/app/repos/page.tsx
+++ src/app/repos/page.tsx
@@ -3,28 +3,49 @@ import { authOptions } from '@/authOptions';
 import ReposPageHeader from '@/app/repos/_components/ReposPageHeader';
 import UserCard from '@/app/repos/_components/UserCard';
 import RepositoryList from '@/app/repos/_components/RepositoryList';
-import RepositoryActions from '@/app/repos/_components/RepositoryActions';
 import NonLoginReposPage from '@/app/repos/_components/NonLoginReposPage';
 
 export default async function ReposPage({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Record<string, string>;
 }) {
   const session = await getServerSession(authOptions);
+  const userId = session?.user.id;
+  const userName = session?.user.login;
 
   if (!session) {
     return <NonLoginReposPage />;
   }
 
+  if (!session?.user) {
+    return <div>Loading...</div>;
+  }
+
+  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/repositories`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userId, userName }),
+  });
+
+  const sortOption = searchParams.sortOption || '최신순';
+  const query = `userId=${userId}&sortOption=${sortOption}`;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/repositories?${query}`);
+  const data = await res.json();
+
   return (
     <div className="mx-auto bg-opacity-70 bg-[url('/images/headerBackground.svg')] bg-contain bg-top bg-no-repeat">
       <div className="mx-auto flex w-[131.4rem] min-w-[131.4rem] flex-col items-center gap-[12.4rem]">
         <ReposPageHeader />
-        <div className="flex flex-col gap-[2.8rem]">
-          <UserCard />
-          <RepositoryActions />
-          <RepositoryList searchParams={searchParams} />
+        <div className="mb-[15rem] flex flex-col gap-[2.8rem]">
+          <UserCard user={session.user} />
+          <RepositoryList
+            user={session.user}
+            repositories={data.repositories}
+            searchParams={searchParams}
+          />
         </div>
       </div>
     </div>
