2214641e885130593192e2c5f0913a4670335c6a
Young2un <popduddms@gmail.com>
2024-09-13 13:23:07 +0900

    Fix: 환경변수수정

--- src/apis/octokit.ts
+++ src/apis/octokit.ts
@@ -1,6 +1,6 @@
 import { Octokit } from 'octokit';
 
-const { OCTOKIT_TOKEN } = process.env;
+const OCTOKIT_TOKEN = process.env.NEXT_PUBLIC_OCTOKIT_TOKEN;
 
 if (!OCTOKIT_TOKEN) {
   throw new Error('.env 파일의 git hub token이 잘못되었습니다.');
