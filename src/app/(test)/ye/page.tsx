f720389437d07baf98760ff12dcbbcfdcac6f426
Young2un <popduddms@gmail.com>
2024-09-16 02:08:38 +0900

    Feat: isScrapped필드 추가

--- src/app/(test)/ye/page.tsx
+++ src/app/(test)/ye/page.tsx
@@ -1,3 +1,28 @@
-export default function TestPage() {
-  return <div>test</div>;
+import { db } from '@/firebase/firebaseConfig';
+import { collection, doc, getDocs, updateDoc } from 'firebase/firestore';
+
+async function addIsScrappedFieldToExistingDocs() {
+  const querySnapshot = await getDocs(collection(db, 'vulDb'));
+
+  querySnapshot.forEach(async document => {
+    const documentRef = doc(db, 'vulDb', document.id);
+
+    await updateDoc(documentRef, {
+      isScrapped: false,
+    });
+  });
+
+  return 'All documents updated with isScrapped: false';
+}
+
+export default async function HomePage() {
+  // 서버 컴포넌트에서 Firebase 데이터 업데이트 호출
+  const message = await addIsScrappedFieldToExistingDocs();
+
+  return (
+    <div>
+      <h1>Scrapped Field Update</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
