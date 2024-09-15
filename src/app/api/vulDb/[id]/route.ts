9148d5c03662c4212d66a1ba5a2816da91430e82
Young2un <popduddms@gmail.com>
2024-09-16 04:50:57 +0900

    Refactor: 초깃값 삭제 및 데이터가 없을시 예외처리

--- src/app/api/vulDb/[id]/route.ts
+++ src/app/api/vulDb/[id]/route.ts
@@ -1,34 +1,26 @@
 import { db } from '@/firebase/firebaseConfig';
-import { ArticleData, CrawlingData } from '@/types/crawlingData';
+import { CrawlingData } from '@/types/crawlingData';
 import { doc, getDoc } from 'firebase/firestore';
 import { NextResponse } from 'next/server';
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
   try {
-    let articleDetailData: ArticleData = {
-      id: '',
-      title: '',
-      keyword: '',
-      uploadDate: {
-        seconds: 0,
-        nanoseconds: 0,
-      },
-      scrapDate: new Date(0),
-      content: [],
-      views: 0,
-    };
     const articleRef = doc(db, 'vulDb', params.id);
     const articleSnap = await getDoc(articleRef);
 
-    if (articleSnap.exists()) {
-      articleDetailData = { ...(articleSnap.data() as CrawlingData), id: params.id };
-      return NextResponse.json(articleDetailData);
-    } else {
-      return NextResponse.json({ message: '데이터가 존재하지 않습니다' }, { status: 500 });
+    if (!articleSnap.exists()) {
+      return NextResponse.json({ message: '데이터가 존재하지 않습니다' }, { status: 404 });
     }
+
+    const articleDetailData = { ...(articleSnap.data() as CrawlingData), id: params.id };
+    return NextResponse.json(articleDetailData);
   } catch (error) {
-    if (error instanceof Error) {
-      return Response.json({ message: error.message, status: false });
-    }
+    return NextResponse.json({
+      message:
+        error instanceof Error
+          ? error.message
+          : '취약점 db 디테일을 가져오는데 오류가 발생했습니다.',
+      status: false,
+    });
   }
 }
