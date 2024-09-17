9e05a5c68efc2610172dd934edb79808d03217ad
Young2un <popduddms@gmail.com>
2024-09-18 07:46:02 +0900

    Refactor: Effect지우고 서버에서 동작하게 수정

--- src/app/api/vulDb/route.ts
+++ src/app/api/vulDb/route.ts
@@ -8,6 +8,7 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const page = parseInt(searchParams.get('page') || '1', 10);
     const label = searchParams.get('label');
+    const userId = searchParams.get('userId');
     const data: ArticleData[] = [];
 
     let vulDbQuery = query(collection(db, 'vulDb'), orderBy('view', 'desc'));
@@ -20,17 +21,28 @@ export async function GET(request: Request) {
 
     const results = await getDocs(vulDbQuery);
 
+    const scrappedArticleIds: string[] = [];
+    if (userId) {
+      const scrapCollection = collection(db, 'users', userId.toString(), 'scrap');
+      const scrapDocs = await getDocs(scrapCollection);
+      scrappedArticleIds.push(...scrapDocs.docs.map(doc => doc.id));
+    }
+
     results.docs.slice((page - 1) * 5, page * 5).forEach(doc => {
       const docData = doc.data() as CrawlingData;
-      data.push({ ...docData, id: doc.id });
+      data.push({
+        ...docData,
+        id: doc.id,
+        isScrapped: scrappedArticleIds.includes(doc.id), // 스크랩 여부 추가
+      });
     });
 
     return NextResponse.json({ data, totalLength: results.size });
   } catch (error) {
     if (error instanceof Error) {
-      return Response.json({ message: error.message, status: false });
+      return NextResponse.json({ message: error.message, status: false });
     }
-    return Response.json({
+    return NextResponse.json({
       message: '알 수 없는 오류가 발생했습니다.',
       status: false,
     });
