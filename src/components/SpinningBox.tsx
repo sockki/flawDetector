5fe99937a978e7ef1dac04146c0acbf8f8270671
printjin <printjin@gmail.com>
2024-09-18 21:54:12 +0900

    Refactor: 박스 코드 수정

--- src/components/SpinningBox.tsx
+++ src/components/SpinningBox.tsx
@@ -1,39 +1,28 @@
 export default function SpinningBox() {
+  const numBoxes = 8;
+  const boxSize = '20rem';
+  const translateDistance = '40vh';
+
   return (
     <div className="relative flex h-screen items-center justify-center">
       <div className="relative h-screen max-h-[100vh] w-screen max-w-[100vh] rounded-full">
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(0deg) translate(40vh)', animationDelay: '0s' }}
-        />
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(45deg) translate(40vh)', animationDelay: '-3.75s' }}
-        />
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(90deg) translate(40vh)', animationDelay: '-7.5s' }}
-        />
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(135deg) translate(40vh)', animationDelay: '-11.25s' }}
-        />
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(180deg) translate(40vh)', animationDelay: '-15s' }}
-        />
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(225deg) translate(40vh)', animationDelay: '-18.75s' }}
-        />
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(270deg) translate(40vh)', animationDelay: '-22.5s' }}
-        />
-        <div
-          className="absolute h-[20rem] w-[20rem] animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
-          style={{ transform: 'rotate(315deg) translate(40vh)', animationDelay: '-26.25s' }}
-        />
+        {Array.from({ length: numBoxes }, (_, index) => {
+          const rotation = index * (360 / numBoxes);
+          const delay = -(index * (30 / numBoxes));
+
+          return (
+            <div
+              key={index}
+              className="absolute animate-rotate-around rounded-xl border-2 border-primary-500 bg-transparent"
+              style={{
+                width: boxSize,
+                height: boxSize,
+                transform: `rotate(${rotation}deg) translate(${translateDistance})`,
+                animationDelay: `${delay}s`,
+              }}
+            />
+          );
+        })}
       </div>
     </div>
   );
