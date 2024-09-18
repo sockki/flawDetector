9a87c22f7b8a66706e4e908a9cfd78acc39f1851
chaduhwan <enghks2973@naver.com>
2024-09-18 23:22:49 +0900

    Feat: 선택된 파일이 존재하지 않을 때 예외처리

--- src/app/repos/[userName]/[repoName]/_components/MultipleSelectModal.tsx
+++ src/app/repos/[userName]/[repoName]/_components/MultipleSelectModal.tsx
@@ -24,20 +24,26 @@ export function MultipleSelectModal({
       setIsModalOpen={onHandleModalOpen}
       isOpen={isModalOpen}
     >
-      <ModalTitle size="sm">선택된 파일을 검사하시겠습니까?</ModalTitle>
-      <CheckedFileList checkedData={modalData} />
-      <Modal.Button
-        buttonText={{ left: '취소', right: '검사하기' }}
-        variant="doubleButton"
-        onClick={{
-          left: () => {
-            onHandleModalOpen();
-          },
-          right: () => {
-            onMultipleCodeScan();
-          },
-        }}
-      />
+      {modalData.length > 0 ? (
+        <>
+          <ModalTitle size="sm">선택된 파일을 검사하시겠습니까?</ModalTitle>
+          <CheckedFileList checkedData={modalData} />
+          <Modal.Button
+            buttonText={{ left: '취소', right: '검사하기' }}
+            variant="doubleButton"
+            onClick={{
+              left: () => {
+                onHandleModalOpen();
+              },
+              right: () => {
+                onMultipleCodeScan();
+              },
+            }}
+          />
+        </>
+      ) : (
+        <Modal.Title size="lg"> 선택된 파일이 없습니다. </Modal.Title>
+      )}
     </Modal>
   );
 }
