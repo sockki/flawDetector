77fe77aa3dd15dc90ca903f7040e2f336591d8dc
Young2un <popduddms@gmail.com>
2024-09-19 13:44:18 +0900

    Fix: 로그인 ui 수정

--- src/app/login/_component/Login.tsx
+++ src/app/login/_component/Login.tsx
@@ -2,21 +2,11 @@
 
 import Button from '@/components/Button/Button';
 import { Ellipse } from '@/components/Ellipse';
-import LogoLoading from '@/components/common/Loading/LogoLoading';
-import { signIn, useSession } from 'next-auth/react';
+import { GithubCatIcon } from '@/public/index';
+import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 
 export default function Login() {
-  const { status } = useSession();
-
-  if (status === 'loading') {
-    return (
-      <div>
-        <LogoLoading />
-      </div>
-    );
-  }
-
   const handleGithubLoginButton = () => {
     signIn('github');
   };
@@ -24,19 +14,23 @@ export default function Login() {
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center">
       <Ellipse />
-      <div className="flex min-h-screen items-center justify-center gap-[24.6rem]">
+      <div className="flex min-h-screen flex-col items-center justify-center gap-[10rem]">
         <div className="flex flex-col items-center gap-[2rem]">
           <h1 className="text-[6rem] font-regular text-primary-500">Find your Flaw,</h1>
           <h2 className="w-fit rounded-full border-[0.4rem] border-primary-500 bg-white px-[4rem] text-[6rem] font-regular text-primary-500">
             Login
           </h2>
         </div>
-        <Button type="button" onClick={handleGithubLoginButton}>
-          GitHub로 연동 로그인하기
-        </Button>
-        <Button asChild>
-          <Link href="https://github.com/">Github</Link>
-        </Button>
+        <div className="flex flex-col gap-[4rem]">
+          <Button type="button" variant="filled" onClick={handleGithubLoginButton}>
+            <GithubCatIcon className="mr-[1.6rem] fill-white" />
+            GitHub로 로그인하기
+          </Button>
+          <Button asChild variant="tonal">
+            <GithubCatIcon className="mr-[1.6rem] fill-primary-400" />
+            <Link href="https://github.com/">Github로 이동하기</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
